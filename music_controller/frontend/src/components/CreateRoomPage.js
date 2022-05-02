de9a71d1b74691da786cdf6134f3a8b199d17c84
jinseong <jinseong205@gmail.com>
2022-05-02 21:20:08 +0900

    ^^7

--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -15,12 +15,46 @@ class CreateRoomPage extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      guestCanPause: true,
+      votesToSkip: this.defaultVotes,
+    };
+
+    this.handleRoomButtonPressed = this.handleRoomButtonPressed.bind(this);
+    this.handleVotesChange = this.handleVotesChange.bind(this);
+    this.handleGuestCanPauseChange = this.handleGuestCanPauseChange.bind(this);
   }
 
+  handleVotesChange(e) {
+    this.setState({
+      votesToSkip: e.target.value,
+    });
+  }
 
+  handleGuestCanPauseChange(e){
+    this.setState({
+      guestCanPause : e.target.value === 'true' ? true : false,
+    })
+  }
 
+  handleRoomButtonPressed(){
+    /*console.log(this.state);*/
+    const requestOptions = {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        votes_to_skip: this.state.votesToSkip,
+        guest_can_pause: this.state.guestCanPause,
+      }),
+    };
+    fetch('/api/create-room', requestOptions).then((response)=>
+    response.json()
+    ).then((data)=> console.log(data));
+  }
+  
   render() {
     return (
+      <div>
       <Grid container spacing={1}>
         <Grid item xs={12} align="center">
           <Typography component="h4" variant="h4">
@@ -30,7 +64,7 @@ class CreateRoomPage extends Component {
         <Grid item xs={12} align="center">
           <FormControl component="fieldset">
             <FormHelperText>
-              <div align="center">Guest Control of Playback State</div>
+              Guest Control of Playback State
             </FormHelperText>
             <RadioGroup
               row
@@ -65,11 +99,12 @@ class CreateRoomPage extends Component {
               }}
             />
             <FormHelperText>
-              <div align="center">Votes Required To Skip Song</div>
+              Votes Required To Skip Song
             </FormHelperText>
           </FormControl>
         </Grid>
         <Grid item xs={12} align="center">
+          {/* 룸 생성 버튼*/}
           <Button
             color="primary"
             variant="contained"
@@ -78,13 +113,14 @@ class CreateRoomPage extends Component {
             Create A Room
           </Button>
         </Grid>
+        {/* 취소 버튼 - router 설정*/}
         <Grid item xs={12} align="center">
           <Button color="secondary" variant="contained" to="/" component={Link}>
             Back
           </Button>
-          
         </Grid>
       </Grid>
+      </div>
     );
   }
 }
