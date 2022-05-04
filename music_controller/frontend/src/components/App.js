6a6a89e7fa03acc03cfd014eb60a9201b66167af
jinseong <jinseong205@gmail.com>
2022-05-05 00:58:38 +0900

    ^^7

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,19 +1,19 @@
-import React, {Component} from "react";
-import {render} from "react-dom"
-import HomePage from "./HomePage"
+import React, { Component } from "react";
+import { render } from "react-dom";
+import HomePage from "./HomePage";
 
-export default class App extends Component{
-    constructor(props){
-        super(props);
-    }
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-    render(){
-        return( 
-        <div className="center">
-            <HomePage/>
-        </div>
-        )
-    }
+  render() {
+    return (
+      <div className="center">
+        <HomePage />
+      </div>
+    );
+  }
 }
 
 const appDiv = document.getElementById("app");
