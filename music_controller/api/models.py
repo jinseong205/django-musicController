5d1110bc7c7d971026dbebb2c3b47d73153ea501
jinseong <jinseong205@gmail.com>
2022-05-09 18:11:39 +0900

    ^^7

--- music_controller/api/models.py
+++ music_controller/api/models.py
@@ -18,3 +18,4 @@ class Room(models.Model):
     guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=2)
     create_at = models.DateTimeField(auto_now_add=True)
+ 
